8b5b2441765b479c8c873077e1e9f9b221221a21
dawngdiggory <137990191+dawngdiggory@users.noreply.github.com>
2023-06-29 23:24:45 +0700

    Add files via upload
    hehe

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Lệ';
+const giftUrl = 'https://www.tiktok.com/@diggory_dawng/video/7250080165454253320';
+const eventName = 'Happy birthday!';
+const titleCard = 'Pour vous ';
+const contentCard = 'Hi vọng trong mắt cậu đều là ánh sáng, trong nụ cười là thẳng thắn vô tư.Tương lai như thế nào, phải bước tiếp mới biết được. Nhưng trước hết, hãy trở thành người mà cậu mong muốn. Nghiêm khắc với bản thân, khoan dung với người khác.Hi vọng cậu sau này dù cười, dù khóc hay tuỳ ý. Nhưng dám yêu, dám hận, dám theo đuổi. Cố lên nhé, tuổi mới xinh đẹp và rạng rỡ! Sinh nhật vui vẻ nhé!... à mà quên nhớ ấn vào tấm ảnh nha có bất ngờ đó :3';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
